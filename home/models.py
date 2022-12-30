1c5df2a1d9d045f0b0b8dfbc1ceb9a346a2e22f3
Aayush <aceraayush@gmail.com>
2022-12-30 09:26:25 +0545

    home page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    logo = models.CharField(max_length= 200)
+    slug = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=300)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    slug = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
+
+class Slider(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField()
+    url = models.URLField(max_length=500,blank = True)
+
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField()
+    rank = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    slug = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+STOCK = (('In stock','In stock'),('Out of stock','Out of stock'))
+LABEL = (('new','new'),('hot','hot'),('sale','sale'),('','default'))
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.IntegerField()
+    discounted_price = models.IntegerField(default = 0)
+    image = models.ImageField(upload_to='media')
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory,on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand,on_delete=models.CASCADE)
+    description = models.TextField()
+    specification = models.TextField()
+    slug = models.TextField()
+    stock = models.CharField(max_length=20,choices=STOCK)
+    label = models.CharField(max_length=20,choices=LABEL)
+
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
-
+from .models import *
 
 class BaseView(View):
     views ={}
 
 class HomeView(BaseView):
     def get(self,request):
-
-        return render(request,'index.html')
+        self.views['categories'] = Category.objects.all()
+        self.views['sliders'] = Slider.objects.all()
+        self.views['brands'] = Brand.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['hot_products'] = Product.objects.filter(label = 'hot')
+        self.views['new_products'] = Product.objects.filter(label='new')
+        self.views['sale_products'] = Product.objects.filter(label='sale')
+        return render(request,'index.html',self.views)
