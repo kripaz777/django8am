d28a03e98869dbfd769a97f2e08b203f36b83c5b
Aayush <aceraayush@gmail.com>
2023-01-03 08:28:18 +0545

    detail view

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('brand/<slug>', BrandView.as_view(), name='brand'),
+    path('detail/<slug>', ProductDetailView.as_view(), name='detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -17,3 +17,24 @@ class HomeView(BaseView):
         self.views['new_products'] = Product.objects.filter(label='new')
         self.views['sale_products'] = Product.objects.filter(label='sale')
         return render(request,'index.html',self.views)
+
+
+class CategoryView(BaseView):
+    def get(self,request,slug):
+        ids = Category.objects.get(slug = slug).id
+        self.views['cat_products'] = Product.objects.filter(category_id = ids)
+
+        return render(request,'category.html',self.views)
+
+
+class BrandView(BaseView):
+    def get(self,request,slug):
+        ids = Brand.objects.get(slug = slug).id
+        self.views['brand_products'] = Product.objects.filter(brand_id = ids)
+
+        return render(request,'brand.html',self.views)
+
+class ProductDetailView(BaseView):
+    def get(self,request,slug):
+        self.views['product_details'] = Product.objects.filter(slug=slug)
+        return render(request,'product-detail.html',self.views)
