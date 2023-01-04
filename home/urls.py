f4b404de99dff7491722f2ec84329a3e72833350
Aayush <aceraayush@gmail.com>
2023-01-04 09:31:10 +0545

    login and signup

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('category/<slug>', CategoryView.as_view(), name='category'),
     path('brand/<slug>', BrandView.as_view(), name='brand'),
     path('detail/<slug>', ProductDetailView.as_view(), name='detail'),
+    path('signup', signup, name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.views import View
 from .models import *
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 class BaseView(View):
     views ={}
 
@@ -40,3 +41,28 @@ class ProductDetailView(BaseView):
         ids = Product.objects.get(slug=slug).subcategory_id
         self.views['related_products'] = Product.objects.filter(subcategory_id=ids)
         return render(request,'product-detail.html',self.views)
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        if password == cpassword:
+            if User.objects.filter(username = username).exists():
+                messages.error(request,'This username is already taken!')
+                return redirect('/signup')
+            elif User.objects.filter(email = email).exists():
+                messages.error(request,'This email is already taken!')
+                return redirect('/signup')
+            else:
+                data = User.objects.create_user(
+                    username = username,
+                    email = email,
+                    password = password
+                )
+                data.save()
+        else:
+            messages.error(request, 'The password does not match!')
+            return redirect('/signup')
+    return render(request,'signup.html')
