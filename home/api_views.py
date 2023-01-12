5208a90bb0288b8476dc3cc8fbe3281cf1f0c6fb
Aayush <aceraayush@gmail.com>
2023-01-12 09:06:23 +0545

    api updated

---
+++ home/api_views.py
@@ -0,0 +1,22 @@
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
+import django_filters.rest_framework
+from rest_framework import generics
+from rest_framework import filters
+
+
+# ViewSets define the view behavior.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    filterset_fields = ['category', 'subcategory','stock','label']
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'discounted_price','id','name']
