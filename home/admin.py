ff16f14764a0e27c3f933cf5c605c4097058c8c6
Aayush <aceraayush@gmail.com>
2023-01-13 09:10:36 +0545

    admin customization

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+admin.site.site_header = "My Ecommerce Shop"
+class ProductA(admin.ModelAdmin):
+    list_display = ('name', 'price','category','subcategory','label','stock')
+    list_filter = ('category','label','stock')
+    search_fields = ('title','description')
+
+
+class Cart_list(admin.ModelAdmin):
+    list_display = ('username', 'items','quantity','total','checkout')
+    list_filter = ('checkout',)
+    search_fields = ('username','slug')
+
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Ad)
 admin.site.register(Brand)
 admin.site.register(Slider)
-admin.site.register(Product)
+admin.site.register(Product,ProductA)
 admin.site.register(ProductReview)
-admin.site.register(Cart)
+admin.site.register(Cart,Cart_list)
 admin.site.register(Review)
--- home/api_views.py
+++ home/api_views.py
@@ -5,6 +5,10 @@ import django_filters.rest_framework
 from rest_framework import generics
 from rest_framework import filters
 
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # ViewSets define the view behavior.
 class ProductViewSet(viewsets.ModelViewSet):
@@ -20,3 +24,32 @@ class ProductListView(generics.ListAPIView):
     filterset_fields = ['category', 'subcategory','stock','label']
     search_fields = ['name', 'description']
     ordering_fields = ['price', 'discounted_price','id','name']
+
+
+class ProductDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
